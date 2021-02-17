b51addc62bc5e08a53e27797beab0a1323490d2c
Rishikesh Sharma <78557013+rishivishwa@users.noreply.github.com>
2021-02-17 09:38:26 -0800

    java c

--- MatchString.java
+++ MatchString.java
@@ -6,25 +6,7 @@ public class MatchString {
      
     public static void main(String [] args){
 
- /* Scanner sc = new Scanner(System.in);
-  //String s = new String();
-  System.out.print(" enter  name: ");
-  
-   String s=sc.nextLine();
-  for(int x=0; x<s.length(); x++){
-     //s = sc.nextLine();
-     s = s + " ";
-     
-     if(s.equals("stop")){
-     System.out.println(s);
-     System.exit(0);
-     break;
-     }
-     else
-     {
-       continue;
-     }
-   }*/
+ 
     Scanner sc = new Scanner(System.in);
     String rishi = "";
     System.out.print(" enter name: ");
@@ -52,12 +34,7 @@ public class MatchString {
 
 
 
-/*if (rishi.length() >= 5)
-    rishi = rishi.substring(0, rishi.length() - 5);
-    System.out.println(rishi);
- */
- 
- 
+
  
 
  
